de431f36610a902343f406efbaadf09206430a3b
SKhatakspeedum <102479212+ShalluKhatak@users.noreply.github.com>
2025-01-05 21:30:48 +0530

    Server setup

--- NodeLearning/server.js
+++ NodeLearning/server.js
@@ -1,1 +1,12 @@
-///Initial commit.
+import http from "http";
+
+const hostname="127.0.0.1";
+const port="8000";
+
+const server = http.createServer((req,res)=>{
+    console.log('req.url :>> ', req.url);
+})
+
+server.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
