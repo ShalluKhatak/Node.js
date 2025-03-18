c0cd46b9ca119dd901bd3cc8de418e2431f80768
Shallu Khatak <102479212+ShalluKhatak@users.noreply.github.com>
2025-03-18 23:50:42 +0530

    DB setup.

--- Express/Project/User_Authentication/Controller/HomeController.js
+++ Express/Project/User_Authentication/Controller/HomeController.js
@@ -1,3 +1,8 @@
-export const HomeController = (req, res) => {
+import { GET_USER } from '../db/query.js';
+import DB_Connection from '../Utils/DB_Connection.js';
+
+export const HomeController = async (req, res) => {
+  const [db_cont] = await DB_Connection.query(GET_USER);
+  console.log('db_cont :>> ', db_cont);
   res.send('Home Screen!');
 };
